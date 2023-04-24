0ab27b528ac71f833dea25e2a774c95cea9f010a
Guilhem Fauré <pro@gfaure.eu>
2023-04-24 16:55:01 +0200

    start syntax conversion

--- spip2md/content.py
+++ spip2md/content.py
@@ -1,6 +1,9 @@
+import re
+
 class content:
     def __init__(self, content):
-        self.content = content
+        self.spip = content
 
     def get_markdown(self):
-        return self.content
+        markdown = self.spip
+        return markdown
