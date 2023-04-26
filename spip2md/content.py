f4195936eb84698e6c3b491398e922dce03b6fe9
Guilhem Fauré <pro@gfaure.eu>
2023-04-26 10:01:09 +0200

    moved test files to dedicated folder, corrected test file to debug grammar

--- spip2md/content.py
+++ spip2md/content.py
@@ -5,6 +5,15 @@ from lark import Lark
 spipParser = Lark(open(path.dirname(__file__) + "/spip.lark"), start="section")
 
 
+# Test
+print("\n--- First test")
+text = open(path.dirname(__file__) + "/../test/1.spip").read()
+print(spipParser.parse(text).pretty())
+print("\n--- Second test")
+text = open(path.dirname(__file__) + "/../test/2.spip").read()
+print(spipParser.parse(text).pretty())
+
+
 class content:
     def __init__(self, content):
         self.spip = content
