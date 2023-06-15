7b2f0d45b8fdc28bf6e6f798556ddb3f9d7d0e8d
Guilhem Fauré <pro@gfaure.eu>
2023-06-15 15:28:37 +0200

    fix licence & add note on each file

--- spip2md/regexmaps.py
+++ spip2md/regexmaps.py
@@ -1,4 +1,18 @@
-# SPIP website to plain Markdown files converter, Copyright (C) 2023 Guilhem Fauré
+"""
+This file is part of spip2md.
+Copyright (C) 2023 LCPQ/Guilhem Fauré
+
+spip2md is free software: you can redistribute it and/or modify it under the terms of
+the GNU General Public License version 2 as published by the Free Software Foundation.
+
+spip2md is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with spip2md.
+If not, see <https://www.gnu.org/licenses/>.
+"""
 # pyright: strict
 from re import I, S, compile
 
